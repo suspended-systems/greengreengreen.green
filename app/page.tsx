9704eb3f4e354eee9108f1399a64bbc704750c67
0xA-10 <tony@natel.li>
2025-04-11 08:47:36 -0700

    green

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { useMemo, useRef, useState } from "react";
+import "./home.css";
+
+import { useEffect, useMemo, useRef, useState } from "react";
 
 import { Toaster } from "@/components/ui/sonner";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
@@ -22,6 +24,8 @@ import { ModeToggle } from "../components/ModeToggle";
 gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
 
 export default function Home() {
+	const [onBoardPosition, setOnBoardPosition] = useState(0);
+
 	const [startValue, setStartValue] = useState(15000);
 	const [startDate, setStartDate] = useState<Date | undefined>(new Date(new Date().setHours(0, 0, 0, 0)));
 	const [endDate, setEndDate] = useState<Date | undefined>();
@@ -54,6 +58,14 @@ export default function Home() {
 		return () => ctx.revert();
 	}, [tab]);
 
+	useEffect(() => {
+		if (onBoardPosition === 0) {
+			// setTimeout(() => {
+			// 	setOnBoardPosition((val) => val + 1);
+			// }, 2500);
+		}
+	}, [onBoardPosition]);
+
 	return (
 		<>
 			<div style={{ position: "absolute", right: 10, top: 10 }}>
@@ -74,52 +86,66 @@ export default function Home() {
 			>
 				green
 			</div>
-			<div
-				style={{
-					border: "1px solid #519c6b",
-					borderLeft: "150px solid transparent",
-					borderRight: "150px solid transparent",
-					position: "relative",
-					top: 1,
-					width: 698,
-					margin: "0 auto",
-				}}
-			/>
-			{/* <span style={{ display: "block",width: "100%", height: 5, color: "#519c6b" }}></span> */}
-			<Tabs defaultValue="calendar" className="testt" onValueChange={setTab}>
-				<TabsList className="grid grid-cols-2" style={{ width: 698, margin: "0 auto" }}>
-					<TabsTrigger value="calendar">Calendar</TabsTrigger>
-					<TabsTrigger value="transactions">Transactions</TabsTrigger>
-				</TabsList>
-				<TabsContent className="tab-content" value="calendar" style={{ marginLeft: "auto", marginRight: "auto" }}>
-					<div>
-						<section className="panel orange">
-							<span className="line2"></span>
-							<CalendarView
-								{...{
-									month,
-									onMonthChange,
-									transactions,
-									startValue,
-									setStartValue,
-									startDate,
-									setStartDate,
-									endDate,
-									setEndDate,
-								}}
-							/>
-						</section>
-					</div>
-				</TabsContent>
-				<TabsContent className="tab-content" value="transactions">
-					<div>
-						<section className="panel orange">
-							<span className="line2"></span>
-							<TransactionsView {...{ columns, transactions, setTransactions, pagination, setPagination }} />
-						</section>
-					</div>
-				</TabsContent>
-			</Tabs>
+			{onBoardPosition != -1 ? (
+				<div
+					className="flex items-center justify-center h-full"
+					style={{ fontSize: 30, marginTop: "calc(50vh - 100px)" }}
+				>
+					{
+						[<p style={{ animationName: "fadeInUp", animationDuration: "2s" }}>Welcome to green!</p>, <></>, <></>][
+							onBoardPosition
+						]
+					}
+				</div>
+			) : (
+				<div>
+					<div
+						style={{
+							border: "1px solid #519c6b",
+							borderLeft: "150px solid transparent",
+							borderRight: "150px solid transparent",
+							position: "relative",
+							top: 1,
+							width: 698,
+							margin: "0 auto",
+						}}
+					/>
+					<Tabs defaultValue="calendar" className="testt" onValueChange={setTab}>
+						<TabsList className="grid grid-cols-2" style={{ width: 698, margin: "0 auto" }}>
+							<TabsTrigger value="calendar">Calendar</TabsTrigger>
+							<TabsTrigger value="transactions">Transactions</TabsTrigger>
+						</TabsList>
+						<TabsContent className="tab-content" value="calendar" style={{ marginLeft: "auto", marginRight: "auto" }}>
+							<div>
+								<section className="panel orange">
+									<span className="line2"></span>
+									<CalendarView
+										{...{
+											month,
+											onMonthChange,
+											transactions,
+											startValue,
+											setStartValue,
+											startDate,
+											setStartDate,
+											endDate,
+											setEndDate,
+										}}
+									/>
+								</section>
+							</div>
+						</TabsContent>
+						<TabsContent className="tab-content" value="transactions">
+							<div>
+								<section className="panel orange">
+									<span className="line2"></span>
+									<TransactionsView {...{ columns, transactions, setTransactions, pagination, setPagination }} />
+								</section>
+							</div>
+						</TabsContent>
+					</Tabs>
+				</div>
+			)}
 			<Toaster />
 		</>
 	);
