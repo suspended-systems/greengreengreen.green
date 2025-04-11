d75ccd84f937ac5f8a146e5c805c4cd54621d8f7
0xA-10 <tony@natel.li>
2025-04-11 06:48:45 -0700

    green

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useMemo, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 
 import { Toaster } from "@/components/ui/sonner";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
@@ -11,6 +11,14 @@ import TransactionsView from "./TransactionsView";
 
 import { columns as columnsData } from "./columns";
 import { myTransactions, Transaction } from "./transactions";
+
+import { gsap } from "gsap";
+
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useIsomorphicLayoutEffect } from "./utils";
+
+gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
 
 export default function Home() {
 	const [startValue, setStartValue] = useState(15000);
@@ -27,31 +35,59 @@ export default function Home() {
 		pageSize: 10,
 	} as PaginationState);
 
+	// refreshes when tab changes
+	const [tab, setTab] = useState("");
+	useIsomorphicLayoutEffect(() => {
+		const ctx = gsap.context(() => {
+			gsap.from(".line-2", {
+				scrollTrigger: {
+					trigger: ".orange",
+					// anticipatePin: 100,
+					pin: true,
+					start: "top top",
+					end: "+=100%",
+				},
+			});
+		});
+
+		return () => ctx.revert();
+	}, [tab]);
+
 	return (
 		<>
 			<div style={{ display: "flex", flexDirection: "column", gap: 32 }}>
-				<Tabs defaultValue="calendar">
+				<Tabs defaultValue="calendar" className="testt" onValueChange={setTab}>
 					<TabsList className="grid w-full grid-cols-2">
 						<TabsTrigger value="calendar">Calendar</TabsTrigger>
 						<TabsTrigger value="transactions">Transactions</TabsTrigger>
 					</TabsList>
-					<TabsContent value="calendar" style={{ marginLeft: "auto", marginRight: "auto" }}>
-						<CalendarView
-							{...{
-								month,
-								onMonthChange,
-								transactions,
-								startValue,
-								setStartValue,
-								startDate,
-								setStartDate,
-								endDate,
-								setEndDate,
-							}}
-						/>
+					<TabsContent className="tab-content" value="calendar" style={{ marginLeft: "auto", marginRight: "auto" }}>
+						<div>
+							<section className="panel orange">
+								<span className="line2"></span>
+								<CalendarView
+									{...{
+										month,
+										onMonthChange,
+										transactions,
+										startValue,
+										setStartValue,
+										startDate,
+										setStartDate,
+										endDate,
+										setEndDate,
+									}}
+								/>
+							</section>
+						</div>
 					</TabsContent>
-					<TabsContent value="transactions">
-						<TransactionsView {...{ columns, transactions, setTransactions, pagination, setPagination }} />
+					<TabsContent className="tab-content" value="transactions">
+						<div>
+							<section className="panel orange">
+								<span className="line2"></span>
+								<TransactionsView {...{ columns, transactions, setTransactions, pagination, setPagination }} />
+							</section>
+						</div>
 					</TabsContent>
 				</Tabs>
 			</div>
--- app/utils.ts
+++ app/utils.ts
@@ -1,5 +1,3 @@
-import { Frequency } from "rrule";
-
 export const DAY_MS = 24 * 60 * 60 * 1000;
 
 export const formatMoney = (amount: number) =>
@@ -9,4 +7,8 @@ export const formatMoney = (amount: number) =>
 	}).format(amount);
 
 export const frequenciesStrings = ["days", "weeks", "months", "years"];
+import { Frequency } from "rrule";
 export const frequencies = [Frequency.DAILY, Frequency.WEEKLY, Frequency.MONTHLY, Frequency.YEARLY];
+
+import { useEffect, useLayoutEffect } from "react";
+export const useIsomorphicLayoutEffect = typeof window !== "undefined" ? useLayoutEffect : useEffect;
