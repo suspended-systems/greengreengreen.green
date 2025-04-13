33d6ed2401385687e178be9007690b426b60dce9
0xA-10 <tony@natel.li>
2025-04-13 13:59:20 -0700

    fix: attempt to improve tour flickering

--- app/Tour.tsx
+++ app/Tour.tsx
@@ -12,27 +12,33 @@ const TOUR_STEPS = [
 		target: ".tour-calendar",
 		content: "Each calendar day displays your projected balance and transactions",
 		placement: "left" as const,
+		disableBeacon: true,
 	},
 	{
 		target: ".tour-calendar-today",
 		content: "Click on a day to see its transactions",
+		disableBeacon: true,
 	},
 	{
 		target: ".tour-calendar-selected-day-details",
 		content: "The selected day's transactions show up here",
 		placement: "right" as const,
+		disableBeacon: true,
 	},
 	{
 		target: ".tour-transactions",
 		content: "Manage your transactions here",
+		disableBeacon: true,
 	},
 	{
 		target: ".tour-add-transaction",
 		content: "Add a transaction here",
+		disableBeacon: true,
 	},
 	{
 		target: ".tour-edit-transaction",
 		content: "You can edit or remove a transaction here",
+		disableBeacon: true,
 	},
 ];
 
