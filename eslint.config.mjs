51b47e6d887fb096e68a20242129bc0d9b7d955a
0xA-10 <tony@natel.li>
2025-04-13 02:43:40 -0700

    allow unused vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,12 +10,11 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-	...compat.extends("next/core-web-vitals", "next/typescript"),
-	{
+	...compat.extends("next/core-web-vitals", "next/typescript", {
 		rules: {
 			"no-unused-vars": "off",
 		},
-	},
+	}),
 ];
 
 export default eslintConfig;
