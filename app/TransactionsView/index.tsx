f06855293ad255dbb306340689230278dd9ccaff
0xA-10 <tony@natel.li>
2025-06-06 11:44:21 -0700

    fix: center info panel text

--- app/TransactionsView/index.tsx
+++ app/TransactionsView/index.tsx
@@ -105,16 +105,18 @@ export function TransactionsTable<TData, TValue>({
 						<>
 							{!session ? (
 								<>
-									<p>
+									<p className="text-center">
 										You are in demo mode. <span className="font-medium">Data will not save.</span>
 									</p>
-									<p>Set up with Google Sheets to store your transactions.</p>
+									<p className="text-center">Set up with Google Sheets to store your transactions.</p>
 									<SetUpWithGoogleSheetsButton />
 								</>
 							) : (
 								<>
-									<p className="text-muted-foreground max-w-[600px]">⚠️ Data will not save until setup is complete.</p>
-									<p className="text-muted-foreground max-w-[600px]">
+									<p className="text-center text-muted-foreground max-w-[600px]">
+										⚠️ Data will not save until setup is complete.
+									</p>
+									<p className="text-center text-muted-foreground max-w-[600px]">
 										❗️ Make sure you are signed in to the same Google Account across green and Sheets.
 									</p>
 									<div className="prose">
