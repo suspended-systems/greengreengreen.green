b3ebc84166d4c828fad2b87b64c996e1e908a276
0xA-10 <tony@natel.li>
2025-06-13 13:19:38 -0700

    feat: langchain

--- components/ChatWindow.tsx
+++ components/ChatWindow.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, FormEvent } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { v4 as uuid } from "uuid";
 import { ArrowUpIcon, TriangleAlertIcon } from "lucide-react";
 
