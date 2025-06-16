bff13fdcd3db0c7d4966d92c0f7a4bb9868b0e2f
0xA-10 <tony@natel.li>
2025-06-16 14:51:57 -0700

    fix: chat window persistence when tabbing out

--- app/SessionProvider.tsx
+++ app/SessionProvider.tsx
@@ -2,5 +2,13 @@
 import { SessionProvider as NextAuthProvider } from "next-auth/react";
 
 export default function SessionProvider({ children }: { children: React.ReactNode }) {
-	return <NextAuthProvider>{children}</NextAuthProvider>;
+	return (
+		<NextAuthProvider
+			// disable automatic re-validation on window focus
+			// this fixes chat window resetting on tab out
+			refetchOnWindowFocus={false}
+		>
+			{children}
+		</NextAuthProvider>
+	);
 }
